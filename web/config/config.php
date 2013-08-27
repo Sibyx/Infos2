5641ac0e37ecef17d9fa2ec9f52ccd8ac560e2d0
Jakub Dubec <jakub.dubec@gmail.com>
2013-08-27 10:20:37 +0200

    CSS Font Fix
    Fixed loading fonts under SSL

--- web/config/config.php
+++ web/config/config.php
@@ -1,7 +1,22 @@
 <?php
 $configs = array();
+
+/*
+ * -- Database configuration --
+ * Supported servers: MySQL, MariaDB
+*/
 $configs['db_host'] = 'localhost';
 $configs['db_user'] = 'user';
 $configs['db_pass'] = 'password';
 $configs['db_database'] = 'infos2';
+
+/*
+ * -- Logging configuration --
+ * log_level = INF | WAR | ERR
+ * 	- INF: Informations, warnings and errors
+ * 	- WAR: warnings and errors
+ *	- ERR: errors only
+*/
+$configs['log_level'] = 'INF';
+
 ?>
