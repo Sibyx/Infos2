e4566109ca7ab0c29bf09462fc938bf5d418cd36
Jakub Dubec <jakub.dubec@gmail.com>
2013-09-06 22:34:56 +0200

    Renderer fix

--- web/index.php
+++ web/index.php
@@ -20,6 +20,7 @@
 	$registry->createAndStoreObject('urlprocessor', 'url');
 	$registry->createAndStoreObject('googleApi', 'google');
 	$registry->createAndStoreObject('authenticate', 'auth');
+	$registry->createAndStoreObject('renderer', 'render');
 	$registry->createAndStoreObject('template', 'template');
 	$registry->getObject('url')->getURLData();
 	$controllers = array();
---
+++ web/registry/renderer.class.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * 16.03.2013
+ * Class Renderer v1.1
+ * Generuje HTML
+*/
+class Renderer {
+
+    public function __construct(Registry $registry) {
+        $this->registry = $registry;
+    }
+
+    public function createUserPanel() {
+        $result = '';
+        if ($this->registry->getObject('auth')->isLoggedIn()) {
+            $result = '<section class="row">' . "\n";
+            $result .= '<div class="large-12 columns">' . "\n";
+            $result .= '<header><h2><small>' . $this->registry->getObject('auth')->getUser()->getFullName() . '</small></h2></header>' . "\n";
+            $result .= '<ul class="side-nav">' . "\n";
+            $result .= '<li><a href="' . $this->registry->getSetting('siteurl') . '/articles/new">New Article</a></li>' . "\n";
+            $result .= '<li><a href="' . $this->registry->getSetting('siteurl') . '/articles/manage">Manage Articles</a></li>' . "\n";
+            $result .= '<li><hr /></li>' . "\n";
+            $result .= '<li><a href="' . $this->registry->getSetting('siteurl') . '/authenticate/logout">Logout</a></li>' . "\n";
+            $result .= '</ul>' . "\n";
+            $result .= '</div>' . "\n";
+            $result .= '</section>' . "\n";
+        }
+        return $result;
+    }
+}
+?>
