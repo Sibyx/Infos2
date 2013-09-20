3a0427f232557c2b7fe30ef0b5e069b8a5d7dc80
Jakub Dubec <jakub.dubec@gmail.com>
2013-09-20 08:39:33 +0200

    event model & controller + preparation for suploExists

--- web/views/default/templates/viewSuplo.tpl.php
+++ web/views/default/templates/viewSuplo.tpl.php
@@ -1,4 +1,11 @@
 <h1>Infos}</h1>
+<header class="row">
+    <div class="large-12 columns">
+        <form action="{siteurl}/suplo/getSuploTable" name="formSuploFilter" id="formSuploFilter" method="post">
+            <input id="suploFilter_date" name="suploFilter_date" type="text" required />
+        </form>
+    </div>
+</header>
 <section class="row">
 	<div class="large-12 collumns">
 		<header><h2>Suplovanie {dateFormated}</h2></header>
@@ -20,3 +27,26 @@
 		</table>
 	</div>
 </section>
+
+<script>
+    var Event = function(text, className) {
+        this.text = text;
+        this.className = className;
+    };
+
+    var events = {};
+    events[new Date("02/14/2011")] = new Event("Valentines Day", "pink");
+    events[new Date("02/18/2011")] = new Event("Payday", "green");
+    //TODO: spravit cez ajax, nie cez php array;
+    $("#suploFilter_date").datepicker({
+        beforeShowDay: function(date) {
+            var event = events[date];
+            if (event) {
+                return [true, event.className, event.text];
+            }
+            else {
+                return [true, '', ''];
+            }
+        }
+    });
+</script>
