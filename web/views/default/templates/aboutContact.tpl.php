3fdc39dc68ce2eb782773df6a5ded4a7798b7e19
Jakub Dubec <jakub.dubec@gmail.com>
2014-03-31 19:59:44 +0200

    Delete aboutContact.tpl.php

--- web/views/default/templates/aboutContact.tpl.php
+++
@@ -1,8 +0,0 @@
-<section class="row">
-	<div class="large-12 columns">
-		<header><h2>Kontakt</h2></header>
-		<form name="formContact" action="{siteurl}/about/contact" method="post">
-
-		</form>
-	</div>
-</section>
